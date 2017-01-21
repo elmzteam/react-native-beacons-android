bba60b25718b7557a61ea312f620540d6253c999
bluepichu <github@bluepichu.com>
2017-01-21 11:56:43 -0500

    First stab at advertising

--- index.js
+++ index.js
@@ -6,83 +6,93 @@ const PARSER_IBEACON = 'm:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24'
 const PARSER_ESTIMOTE = 'm:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24'
 
 const tramissionSupport = {
-  0: 'SUPPORTED',
-  1: 'NOT_SUPPORTED_MIN_SDK',
-  2: 'NOT_SUPPORTED_BLE',
-  3: 'DEPRECATED_NOT_SUPPORTED_MULTIPLE_ADVERTISEMENTS',
-  4: 'NOT_SUPPORTED_CANNOT_GET_ADVERTISER',
-  5: 'NOT_SUPPORTED_CANNOT_GET_ADVERTISER_MULTIPLE_ADVERTISEMENTS'
+	0: 'SUPPORTED',
+	1: 'NOT_SUPPORTED_MIN_SDK',
+	2: 'NOT_SUPPORTED_BLE',
+	3: 'DEPRECATED_NOT_SUPPORTED_MULTIPLE_ADVERTISEMENTS',
+	4: 'NOT_SUPPORTED_CANNOT_GET_ADVERTISER',
+	5: 'NOT_SUPPORTED_CANNOT_GET_ADVERTISER_MULTIPLE_ADVERTISEMENTS'
 }
 
 const setHardwareEqualityEnforced = (e) => {
-  beaconsAndroid.setHardwareEqualityEnforced(e)
+	beaconsAndroid.setHardwareEqualityEnforced(e)
 }
 
 const detectIBeacons = () => {
-  beaconsAndroid.addParser(PARSER_IBEACON)
+	beaconsAndroid.addParser(PARSER_IBEACON)
 }
 
 const detectEstimotes = () => {
-  beaconsAndroid.addParser(PARSER_ESTIMOTE)
+	beaconsAndroid.addParser(PARSER_ESTIMOTE)
 }
 
 const detectCustomBeaconLayout = (parser) => {
-  beaconsAndroid.addParser(parser)
+	beaconsAndroid.addParser(parser)
 }
 
 const setBackgroundScanPeriod = (period) => {
-  beaconsAndroid.setBackgroundScanPeriod(period)
+	beaconsAndroid.setBackgroundScanPeriod(period)
 }
 
 const setBackgroundBetweenScanPeriod = (period) => {
-  beaconsAndroid.setBackgroundBetweenScanPeriod(period)
+	beaconsAndroid.setBackgroundBetweenScanPeriod(period)
 }
 
 const setForegroundScanPeriod = (period) => {
-  beaconsAndroid.setForegroundScanPeriod(period)
+	beaconsAndroid.setForegroundScanPeriod(period)
 }
 
 const getRangedRegions = () => new Promise((resolve, reject) => {
-  beaconsAndroid.getRangedRegions(resolve)
+	beaconsAndroid.getRangedRegions(resolve)
 })
 
 const getMonitoredRegions = () => new Promise((resolve, reject) => {
-  beaconsAndroid.getMonitoredRegions(resolve)
+	beaconsAndroid.getMonitoredRegions(resolve)
 })
 
 const checkTransmissionSupported = () => new Promise((resolve, reject) => {
-  beaconsAndroid.checkTransmissionSupported(status => resolve(tramissionSupport[status]))
+	beaconsAndroid.checkTransmissionSupported(status => resolve(tramissionSupport[status]))
 })
 
 const startMonitoringForRegion = (region) => new Promise((resolve, reject) => {
-  beaconsAndroid.startMonitoring(region.identifier, region.uuid, region.minor, region.major, resolve, reject)
+	beaconsAndroid.startMonitoring(region.identifier, region.uuid, region.minor, region.major, resolve, reject)
 })
 
 const startRangingBeaconsInRegion = (regionId, beaconsUUID) => new Promise((resolve, reject) => {
-  beaconsAndroid.startRanging(regionId, beaconsUUID, resolve, reject)
+	beaconsAndroid.startRanging(regionId, beaconsUUID, resolve, reject)
 })
 
 const stopMonitoringForRegion = (region) => new Promise((resolve, reject) => {
-  beaconsAndroid.stopMonitoring(region.identifier, region.uuid, region.minor, region.major, resolve, reject)
+	beaconsAndroid.stopMonitoring(region.identifier, region.uuid, region.minor, region.major, resolve, reject)
 })
 
 const stopRangingBeaconsInRegion = (regionId, beaconsUUID) => new Promise((resolve, reject) => {
-  beaconsAndroid.stopRanging(regionId, beaconsUUID, resolve, reject)
+	beaconsAndroid.stopRanging(regionId, beaconsUUID, resolve, reject)
+})
+
+const startAdvertising = (region) => new Promise((resolve, reject) => {
+	beaconsAndroid.startTransmitting(region.identifier, region.uuid, region.major, region.minor, resolve, reject)
+})
+
+const stopAdvertising = () => new Promise((resolve, reject) => {
+	beaconsAndroid.stopTransmitting(resolve, reject)
 })
 
 module.exports = {
-  setHardwareEqualityEnforced,
-  detectIBeacons,
-  detectEstimotes,
-  detectCustomBeaconLayout,
-  setBackgroundScanPeriod,
-  setBackgroundBetweenScanPeriod,
-  setForegroundScanPeriod,
-  checkTransmissionSupported,
-  getRangedRegions,
-  getMonitoredRegions,
-  startMonitoringForRegion,
-  startRangingBeaconsInRegion,
-  stopMonitoringForRegion,
-  stopRangingBeaconsInRegion
+	setHardwareEqualityEnforced,
+	detectIBeacons,
+	detectEstimotes,
+	detectCustomBeaconLayout,
+	setBackgroundScanPeriod,
+	setBackgroundBetweenScanPeriod,
+	setForegroundScanPeriod,
+	checkTransmissionSupported,
+	getRangedRegions,
+	getMonitoredRegions,
+	startMonitoringForRegion,
+	startRangingBeaconsInRegion,
+	stopMonitoringForRegion,
+	stopRangingBeaconsInRegion,
+	startAdvertising,
+	stopAdvertising
 }
